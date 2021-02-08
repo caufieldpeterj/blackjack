2bcf4dc3f68216662b7bbf8c9c503f390bc2586c
peter caufield <caufield.peterj@gmail.com>
2021-02-08 15:57:49 -0500

    created functions for shuffling, and dealing. confirmed with Phil that the ace should take the value of 11, don't overthink the data structure until we reach the logic

--- app.js
+++ app.js
@@ -22,17 +22,7 @@ Expanded Rules:
                     THE SETUP
 ==============================================
 Overview:  A function that creates the player & dealer objects, and the deck of cards.
-*/
 
-// A dealer, single-player, and a deck of 52 cards exists
-const player = {
-    hand: []
-};
-const dealer = {
-    hand: []
-};
-
-/*
 ====================================================
 FIRST METHOD
 ====================================================
@@ -62,50 +52,7 @@ player.firstCard.value = cardValue;
 
 console.log(player);
 
-// This was too simple, after researching I discovered the Fisher-Yates shuffle algorithm... which leads to the.......
-
-====================================================
-SECOND METHOD
-====================================================
-
-// I think we should create card objects vs. using arrays
-// the keys are the actual name of the card (Four, Five, Jack, Queen, King, Ace) 
-// and value is the numerical value of the card.. will allow us to assign an Ace a value of 1 or 11?
-// I found this link below Monday morning at 8:28am, after outlining this method over the weekend/with Raahima (TA)! Hard to believe, but it's the truth.
-// https://www.programiz.com/javascript/examples/shuffle-card
-*/
-
-const suits2 = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
-
-const cards2 = {
-    Two: 2, 
-    Three: 3,
-    Four: 4, 
-    Five: 5, 
-    Six: 6,
-    Seven: 7, 
-    Eight: 8, 
-    Nine: 9, 
-    Ten: 10,
-    Jack: 10,
-    Queen: 10,
-    King: 10, 
-    Ace: {
-        One: 1,
-        Eleven: 11
-    }
-};
-// for loop to iterate over the suits and for loop to iterate over the cards to create the actual cards
-let deckOfCards2 = [];
-
-for (suits of suits2) {
-    for (values in cards2) {
-        // console.log(values, suits);
-        let card2 = {Rank: values, Suit: suits}
-        deckOfCards2.push(card2);
-    }
-}
-
+// This was too simple, after researching I discovered the Fisher-Yates shuffle algorithm... which leads to the....... SECOND METHOD
 // returns an object  https://www.w3schools.com/js/js_arrays.asp
 // console.log(typeof deckOfCards2)      
 
@@ -115,52 +62,9 @@ for (suits of suits2) {
 // returns 52 cards
 // console.log(deckOfCards2.length);     
 
-/*
-==============================================
-                SHUFFLE 
-==============================================
-
-Overview: shuffle the deck, and deal the player and dealer 2 cards.
-
-https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-    Summary: Fisher-Yates' algo was slightly modified by Richard Durstenfeld in '64 to improve the run-time to O(n), as the former was developed in 1938 using pencil and paper. Feautured in https://en.wikipedia.org/wiki/The_Art_of_Computer_Programming 
-*/
 
 
-// Initializing a function to shuffle the deck
-// https://gist.github.com/webbower/8d19b714ded3ec53d1d7ed32b79fdbac
-const ShuffleDeck = (deck) => {
-    for (let i=deck.length-1;i>0;i--) {
-        let x = Math.floor(Math.random()*(i+1));
-        [deck[i], deck[x]] = [deck[x], deck[i]];
-    }
-}
 
-ShuffleDeck(deckOfCards2);
-// console.log(typeof deckOfCards2);
-// console.log(deckOfCards2);
-
-/*
-==============================================
-                DEAL 
-==============================================
-Deal Sequence
-    - At random, the single-player gets a card from the deck
-    - At random, the dealer gets a card 
-    - At random, the single-player gets another card 
-    - At random, the dealer gets another card (face down)
-*/
-
-// console.log(deckOfCards2, deckOfCards2.length);
-player.hand.push(deckOfCards2.pop());
-dealer.hand.push(deckOfCards2.pop());
-player.hand.push(deckOfCards2.pop());
-dealer.hand.push(deckOfCards2.pop());
-
-// console.log(deckOfCards2.length);
-
-console.log(dealer);
-console.log(player);
 
 
 /*
@@ -186,10 +90,107 @@ console.log(player);
 
 
 
-// onload function
-/*
-(() => {
+$(() => {
+    /*
+    ==============================================
+                        THE SETUP
+    ==============================================
+    Overview:  A function that creates the player & dealer objects, and the deck of cards.A dealer, single-player, and a deck of 52 cards exists
+
+    // I think we should create card objects vs. using arrays
+    // the keys are the actual name of the card (Four, Five, Jack, Queen, King, Ace) 
+    // and value is the numerical value of the card.. will allow us to assign an Ace a value of 1 or 11?
+    // I found this link below Monday morning at 8:28am, after outlining this method over the weekend/with Raahima (TA)! Hard to believe, but it's the truth.
+    // https://www.programiz.com/javascript/examples/shuffle-card
+    */
+
+   const player = {
+       hand: []
+    };
+    const dealer = {
+        hand: []
+    };
+
+    const cardSuits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+
+    const cardRank = {
+        Two: 2, 
+        Three: 3,
+        Four: 4, 
+        Five: 5, 
+        Six: 6,
+        Seven: 7, 
+        Eight: 8, 
+        Nine: 9, 
+        Ten: 10,
+        Jack: 10,
+        Queen: 10,
+        King: 10, 
+        Ace: 11
+    };
+    // console.log(Object.values(cardRank));
+
+    // for loop to iterate over the suits and for loop to iterate over the cards to create the actual cards
+    
+    let deckOfCards = [];
+
+    for (suit of cardSuits) {
+        for (ranks in cardRank) {
+            // console.log(values, suits);
+            // console.log(cardRank[ranks])
+            let card = {Rank: ranks, Value: (cardRank[ranks]), Suit: suit}
+            deckOfCards.push(card);
+        }
+    };
+
+    // console.log(deckOfCards);
+
+    
+   
+    /*
+    ==============================================
+                    SHUFFLE 
+    ==============================================
+
+    Overview: shuffle the deck
+
+    https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+        Summary: Fisher-Yates' algo was slightly modified by Richard Durstenfeld in '64 to improve the run-time to O(n), as the former was developed in 1938 using pencil and paper. Feautured in https://en.wikipedia.org/wiki/The_Art_of_Computer_Programming 
+    */
 
 
-})
-*/
+    // Initializing a function to shuffle the deck
+    // https://gist.github.com/webbower/8d19b714ded3ec53d1d7ed32b79fdbac
+    const ShuffleDeck = (deck) => {
+        for (let i=deck.length-1;i>0;i--) {
+            let x = Math.floor(Math.random()*(i+1));
+            [deck[i], deck[x]] = [deck[x], deck[i]];
+        }
+    }
+
+    ShuffleDeck(deckOfCards);
+    // console.log(typeof deckOfCards2);
+    // console.log(deckOfCards);
+
+    /*
+    ==============================================
+                    DEAL 
+    ==============================================
+    Deal Sequence
+        - At random, the single-player gets a card from the deck
+        - At random, the dealer gets a card 
+        - At random, the single-player gets another card 
+        - At random, the dealer gets another card (face down)
+    */
+    const dealCards = () => {
+        for (let i=0;i<2;i++) {
+            player.hand.push(deckOfCards.pop());
+            dealer.hand.push(deckOfCards.pop());
+        }
+    };
+
+    dealCards();
+    console.log(dealer.hand);
+    console.log(player.hand);
+
+});
