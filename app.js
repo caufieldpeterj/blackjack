530380c9f0d20fcf6ec201a3ab21afc2dcde0a46
peter caufield <caufield.peterj@gmail.com>
2021-02-09 10:10:39 -0500

    successfully declared variables outside of onload function, onto game logic

--- app.js
+++ app.js
@@ -1,228 +1,152 @@
 /*
-================================================
-            BLACKJACK PSEUDOCODE
-================================================
-Name of game: 
-    Blackjack
-
-How to win:
-    Sum of your cards === 21, or are less than 21 but greater than the sum of the dealer's cards
-
-How to lose:
-    sum of your cards > 21, or are less than the sum of your opponent's cards
-
-Expanded Rules: 
-    https://bicyclecards.com/how-to-play/blackjack/
-
-==============================================
-                GAME STRUCTURE
-==============================================
-
 ==============================================
                     THE SETUP
 ==============================================
-Overview:  A function that creates the player & dealer objects, and the deck of cards.
+Overview:  A function that creates the player & dealer objects, and the deck of cards.A dealer, single-player, and a deck of 52 cards exists
 
-====================================================
-FIRST METHOD
-====================================================
+// I think we should create card objects vs. using arrays
+// the keys are the actual name of the card (Four, Five, Jack, Queen, King, Ace) 
+// and value is the numerical value of the card.. will allow us to assign an Ace a value of 1 or 11?
+// I found this link below Monday morning at 8:28am, after outlining this method over the weekend/with Raahima (TA)! Hard to believe, but it's the truth.
+// https://www.programiz.com/javascript/examples/shuffle-card
+    */
 
-// creating the deckOfCards object, where the suits are keys, and values are the card values
-let deckOfCards = {
-    Clubs: [2,3,4,5,6,7,8,9,10,10,10,10,11],
-    Diamonds: [2,3,4,5,6,7,8,9,10,10,10,10,11],
-    Hearts: [2,3,4,5,6,7,8,9,10,10,10,10,11],
-    Spades: [2,3,4,5,6,7,8,9,10,10,10,10,11],
+const player = {
+    hand: []
 };
-// instantiating the suit varibale, assigning it a random suit within the deckOfCards object
-let suit = Object.keys(deckOfCards)[Math.floor(Math.random()*4)];
-console.log(suit);
+const dealer = {
+    hand: []
+};
 
-// instantiating the cardValue varibale, assigning it a random value within the deckOfCards object
-let cardValue = Math.floor(Math.random()*13);
-console.log(cardValue);
+const cardSuits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
 
-// Prints an array of the objects property names (suits, in our case)
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys 
-// console.log(Object.keys(deckOfCards));
+const cardRank = {
+    Two: 2, 
+    Three: 3,
+    Four: 4, 
+    Five: 5, 
+    Six: 6,
+    Seven: 7, 
+    Eight: 8, 
+    Nine: 9, 
+    Ten: 10,
+    Jack: 10,
+    Queen: 10,
+    King: 10, 
+    Ace: 11
+};
+// console.log(Object.values(cardRank));
 
-player.firstCard = {};
-player.firstCard.suits = suit;
-player.firstCard.value = cardValue;
+// for loop to iterate over the suits and for loop to iterate over the cards to create the actual cards
 
-console.log(player);
+let deckOfCards = [];
 
-// This was too simple, after researching I discovered the Fisher-Yates shuffle algorithm... which leads to the....... SECOND METHOD
-// returns an object  https://www.w3schools.com/js/js_arrays.asp
-// console.log(typeof deckOfCards2)      
+for (suit of cardSuits) {
+    for (ranks in cardRank) {
+        // console.log(values, suits);
+        // console.log(cardRank[ranks])
+        let card = {Rank: ranks, Value: (cardRank[ranks]), Suit: suit}
+        deckOfCards.push(card);
+    }
+};
+/*
+==============================================
+                SHUFFLE 
+==============================================
 
-// prints an array of card objects
-// console.log(deckOfCards2)      
+Overview: shuffle the deck
 
-// returns 52 cards
-// console.log(deckOfCards2.length);     
+https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    Summary: Fisher-Yates' algo was slightly modified by Richard Durstenfeld in '64 to improve the run-time to O(n), as the former was developed in 1938 using pencil and paper. Feautured in https://en.wikipedia.org/wiki/The_Art_of_Computer_Programming 
 */
 
+// Initializing a function to shuffle the deck
+// https://gist.github.com/webbower/8d19b714ded3ec53d1d7ed32b79fdbac
+const ShuffleDeck = (deck) => {
+    for (let i=deck.length-1;i>0;i--) {
+        let x = Math.floor(Math.random()*(i+1));
+        [deck[i], deck[x]] = [deck[x], deck[i]];
+    }
+}  
+// console.log(typeof deckOfCards2);
+// console.log(deckOfCards);
+/*
+==============================================
+                DEAL 
+==============================================
+Deal Sequence
+    - At random, the single-player gets a card from the deck
+    - At random, the dealer gets a card 
+    - At random, the single-player gets another card 
+    - At random, the dealer gets another card (face down)
+*/
+// grabbing player and dealer divs with jQuery
+const $playerHand = $('#player-cards');
+const $dealerHand = $('#dealer-cards');
+
+const dealCards = () => {
+    for (let i=0;i<4;i++) {
+        const $card = $('<div>').addClass('cards');
+        if (i%2===0) {
+            player.hand.push(deckOfCards.pop())
+            // $card.text('first card');
+            $playerHand.append($card);
+        } else {
+            dealer.hand.push(deckOfCards.pop());
+            // $card.text('first card')
+            $dealerHand.append($card);
+        }            
+    }
+};
+
+/*
+==============================================
+                    NATURALS
+==============================================
+- If the sum of the player's cards is 21 && the dealers 1st card is not a 10 or Ace... NATURAL BLACKJACK for that player
+
+- If the sum of the player's cards is not 21 AND the dealer's 1st card is a 10 or Ace check to see if the sum of dealer's cards is 21... if so, NATURAL BLACKJACK for the dealer
+
+- If the sum of the player's cards === 21 AND the dealer's 1st card is a 10 or Ace check to see if the sum of dealer's cards is 21... if sum of dealers card is 21... tie game
+*/
+const checkNaturals = () => {
+    let dealerHand = dealer.hand[0].Value + dealer.hand[1].Value;
+    let playerHand = player.hand[0].Value + player.hand[1].Value;
+    // console.log(dealerHand);
+    // console.log(typeof dealerHand);          // returns number
+    console.log("Dealer total: " +dealerHand);
+    console.log("Player total: " +playerHand);
+
+    if (playerHand===21 && dealerHand===21) {
+        let $h3 = $('<h3>').text('DOUBLE BLACKJACK, TIE GAME!');
+        $('body').append($h3);
+    } else if (playerHand===21 && dealerHand < 21) {
+        let $h3 = $('<h3>').text('NATURAL BLACKJACK FOR THE PLAYER!');
+        $('body').append($h3);
+    } else if (playerHand < 21 && dealerHand===21) {
+        let $h3 = $('<h3>').text('NATURAL BLACKJACK FOR THE DEALER!');
+        $('body').append($h3);
+    } else if (playerHand > 21 || dealerHand > 21) {
+        let $h3 = $('<h3>').text('Houston we have a problem');
+        $('body').append($h3);
+    } else {
+        return;
+    }
+};
 
 const hitPlayer = () => {
-    
+    // console.log('the player wants another card');
+    console.log(player.hand);
 }
 
 
 
-
-
-
-
 $(() => {
-
-    /*
-    ==============================================
-                        THE SETUP
-    ==============================================
-    Overview:  A function that creates the player & dealer objects, and the deck of cards.A dealer, single-player, and a deck of 52 cards exists
-
-    // I think we should create card objects vs. using arrays
-    // the keys are the actual name of the card (Four, Five, Jack, Queen, King, Ace) 
-    // and value is the numerical value of the card.. will allow us to assign an Ace a value of 1 or 11?
-    // I found this link below Monday morning at 8:28am, after outlining this method over the weekend/with Raahima (TA)! Hard to believe, but it's the truth.
-    // https://www.programiz.com/javascript/examples/shuffle-card
-    */
-
-    const player = {
-        hand: []
-    };
-    const dealer = {
-        hand: []
-    };
-
-    const cardSuits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
-
-    const cardRank = {
-        Two: 2, 
-        Three: 3,
-        Four: 4, 
-        Five: 5, 
-        Six: 6,
-        Seven: 7, 
-        Eight: 8, 
-        Nine: 9, 
-        Ten: 10,
-        Jack: 10,
-        Queen: 10,
-        King: 10, 
-        Ace: 11
-    };
-    // console.log(Object.values(cardRank));
-
-    // for loop to iterate over the suits and for loop to iterate over the cards to create the actual cards
-
-    let deckOfCards = [];
-
-    for (suit of cardSuits) {
-        for (ranks in cardRank) {
-            // console.log(values, suits);
-            // console.log(cardRank[ranks])
-            let card = {Rank: ranks, Value: (cardRank[ranks]), Suit: suit}
-            deckOfCards.push(card);
-        }
-    };
-
-    // console.log(deckOfCards);
-
-    /*
-    ==============================================
-                    SHUFFLE 
-    ==============================================
-
-    Overview: shuffle the deck
-
-    https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-        Summary: Fisher-Yates' algo was slightly modified by Richard Durstenfeld in '64 to improve the run-time to O(n), as the former was developed in 1938 using pencil and paper. Feautured in https://en.wikipedia.org/wiki/The_Art_of_Computer_Programming 
-    */
-
-    // Initializing a function to shuffle the deck
-    // https://gist.github.com/webbower/8d19b714ded3ec53d1d7ed32b79fdbac
-    const ShuffleDeck = (deck) => {
-        for (let i=deck.length-1;i>0;i--) {
-            let x = Math.floor(Math.random()*(i+1));
-            [deck[i], deck[x]] = [deck[x], deck[i]];
-        }
-    }  
-    // console.log(typeof deckOfCards2);
-    // console.log(deckOfCards);
-    /*
-    ==============================================
-                    DEAL 
-    ==============================================
-    Deal Sequence
-        - At random, the single-player gets a card from the deck
-        - At random, the dealer gets a card 
-        - At random, the single-player gets another card 
-        - At random, the dealer gets another card (face down)
-    */
-    // grabbing player and dealer divs with jQuery
-    const $playerHand = $('#player-cards');
-    const $dealerHand = $('#dealer-cards');
-
-
-    const dealCards = () => {
-        for (let i=0;i<4;i++) {
-            const $card = $('<div>').addClass('cards');
-            if (i%2===0) {
-                player.hand.push(deckOfCards.pop())
-                // $card.text('first card');
-                $playerHand.append($card);
-            } else {
-                dealer.hand.push(deckOfCards.pop());
-                // $card.text('first card')
-                $dealerHand.append($card);
-            }            
-        }
-    };
-    /*
-    ==============================================
-                        NATURALS
-    ==============================================
-    - If the sum of the player's cards is 21 && the dealers 1st card is not a 10 or Ace... NATURAL BLACKJACK for that player
-
-    - If the sum of the player's cards is not 21 AND the dealer's 1st card is a 10 or Ace check to see if the sum of dealer's cards is 21... if so, NATURAL BLACKJACK for the dealer
-
-    - If the sum of the player's cards === 21 AND the dealer's 1st card is a 10 or Ace check to see if the sum of dealer's cards is 21... if sum of dealers card is 21... tie game
-    */
-
-
     ShuffleDeck(deckOfCards);
     dealCards(); 
-
-    const checkNaturals = () => {
-        let dealerHand = dealer.hand[0].Value + dealer.hand[1].Value;
-        let playerHand = player.hand[0].Value + player.hand[1].Value;
-        // console.log(dealerHand);
-        // console.log(typeof dealerHand);          // returns number
-        console.log("Dealer total: " +dealerHand);
-        console.log("Player total: " +playerHand);
-
-        if (playerHand===21 && dealerHand===21) {
-            let $h3 = $('<h3>').text('DOUBLE BLACKJACK, TIE GAME!');
-            $('body').append($h3);
-        } else if (playerHand===21 && dealerHand < 21) {
-            let $h3 = $('<h3>').text('NATURAL BLACKJACK FOR THE PLAYER!');
-            $('body').append($h3);
-        } else if (playerHand < 21 && dealerHand===21) {
-            let $h3 = $('<h3>').text('NATURAL BLACKJACK FOR THE DEALER!');
-            $('body').append($h3);
-        } else if (playerHand > 21 || dealerHand > 21) {
-            let $h3 = $('<h3>').text('Houston we have a problem');
-            $('body').append($h3);
-        } else {
-            return;
-        }
-    };
-
     checkNaturals();
-    console.log(dealer.hand);
-    console.log(player.hand);
+    // console.log(dealer.hand);
+    // console.log(player.hand);
 
     /*
     ==============================================
@@ -237,7 +161,9 @@ $(() => {
     // target the buttons using jQuery and assign them to a $variable
     const $hit = $('#hit');
     // add event listeners to listen for a click, tie that into a function that will proceed to the dealer or draw another card from the deck
-    $hit.on('click', () => {console.log('the player wants another card')});
+    // $hit.on('click', () => {console.log('the player wants another card')});
+    
+    $hit.on('click', hitPlayer);
 
 
 
